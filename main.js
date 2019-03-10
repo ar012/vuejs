c674c46c71d1d626d4d65478654d003e23ead388
Abdur Razzak <abdur.razzak@ctrends-software.com>
2019-03-11 00:01:47 +0600

    project2 added

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'Learning Vue.js'
+  }
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: "Hello Vue! welcome"
+    message: "Hello Vue! welcome..."
   }
 });
+
+// app.message = 'I have changed the data!';
+
+
+var myApp = new Vue({
+  el: '#myapp',
+  data: {
+    text: 'Welcome'
+  }
+});
+
+myApp.text = 'Welcome to';
+
+
+
+
